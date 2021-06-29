3d27149e86a48e7762d62bd6c4066b73337ed8d2
Vu Huy Loc <locloc@gmail.com>
2021-06-29 17:48:33 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Beginner of React
         </a>
       </header>
     </div>
